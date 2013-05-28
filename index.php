736947b5465b6dab17f46ec5e70acb196d3ab843
Yoel Nunez <y.nunez@floms.com>
2013-05-28 00:39:19 -0400

    version: 1.1

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+session_start();
 
 require_once("captcha.php");
 
-
-if(isset($_POST["code"])) {
+if(isset($_POST['rCaptcha'])) {
+	echo captcha::image();
+	exit;
+}
+else if(isset($_POST["code"])) {
 	if($_POST["code"] == captcha::get_code()) {
 		echo "Good";
 	}
@@ -15,8 +19,31 @@ if(isset($_POST["code"])) {
 }
 
 ?>
-<form method="post">
-	<input type="text" name="code" />
-	<img src="<?= captcha::image() ?>" />
-	<input type="submit" value="Check" />
+<script>
+function refreshCaptcha(target) {
+
+    var req = new XMLHttpRequest();
+
+    req.open("POST", window.location, true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	
+    req.onreadystatechange = function() {
+	    if(req.readyState == 4 && req.status == 200) {
+			target.src = req.responseText;
+	    }
+    }
+
+    req.send("rCaptcha=true");
+}
+</script>
+
+<form method="post" autocomplete="off">
+	<fieldset>
+		<legend>PHP Captcha</legend>
+		
+		<input type="text" name="code" placeholder="Captcha Code" /><br/>
+		<img src="<?= captcha::image() ?>" onclick="refreshCaptcha(this)" title="click to refresh" /><br/>
+		<input type="submit" value="Check" /><br/>
+	</fieldset>
 </form>
